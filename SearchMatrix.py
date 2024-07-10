368d98ce80d74fb7f0c4513a6c736cb3d52d9cec
solmaz <67147501+solmaznsr@users.noreply.github.com>
2024-07-10 07:52:42 -0800

    Add files via upload
    task1-serch a target in Matrix

---
+++ SearchMatrix.py
@@ -0,0 +1,34 @@
+def searchMatrix(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+    
+    rows, cols = len(matrix), len(matrix[0])    
+    left, right = 0, rows * cols - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        mid_value = matrix[mid // cols][mid % cols]
+        
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return False
+
+rows = int(input("Enter number of rows: "))
+cols = int(input("Enter number of columns: "))
+
+matrix = []
+print("Enter the elements of the matrix :")
+for i in range(rows):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+target = int(input("Enter the target: "))
+
+goal = searchMatrix(matrix, target)
+print(f"{target} found in matrix: {goal}")
+
